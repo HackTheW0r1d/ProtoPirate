08e2002c35d24723fc0987d3d6fcdda105d066ab
MX <10697207+xMasterX@users.noreply.github.com>
2026-02-10 23:46:43 +0300

    fix sub decode reset

--- scenes/protopirate_scene_sub_decode.c
+++ scenes/protopirate_scene_sub_decode.c
@@ -877,12 +877,14 @@ bool protopirate_scene_sub_decode_on_event(void* context, SceneManagerEvent even
                     uint16_t history_count = protopirate_history_get_item(ctx->history);
 
                     if(history_count > 0) {
+                        subghz_receiver_reset(app->txrx->receiver);
                         ctx->state = DecodeStateShowSuccess;
                         ctx->selected_history_index = 0;
                         ctx->showing_signal_info = false;
                         ctx->result_display_counter = 0;
                         notification_message(app->notifications, &sequence_success);
                     } else {
+                        subghz_receiver_reset(app->txrx->receiver);
                         furi_string_printf(
                             ctx->result,
                             "RAW Signal\n\n"
